94be2ab9ce577ec951a833350763be194d31e7c6
chaya222 <poonam.parth@wellthy.care>
2019-05-05 01:26:00 +0530

    fixed version

--- app/src/main/java/com/poonam/MainActivity.kt
+++ app/src/main/java/com/poonam/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
 //        otpView.addTextChangedListener(object : TextWatcher {
 //            override fun afterTextChanged(s: Editable?) {
 //            }
